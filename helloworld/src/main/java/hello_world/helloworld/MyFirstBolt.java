58488bb522207ccb6c1e1c1e770c6978409183b3
SadkiratSingh <sadkiratsingh@yahoo.com>
2021-12-30 15:36:04 +0530

    bolt created

--- helloworld/src/main/java/hello_world/helloworld/MyFirstBolt.java
+++ helloworld/src/main/java/hello_world/helloworld/MyFirstBolt.java
@@ -1,5 +1,26 @@
 package hello_world.helloworld;
 
-public class MyFirstBolt {
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
 
+public class MyFirstBolt extends BaseBasicBolt {
+
+	@Override
+	public void execute(Tuple input, BasicOutputCollector collector) {
+		// TODO Auto-generated method stub
+		Integer x = input.getInteger(0);
+		x = x*2;
+		collector.emit(new Values(x));
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		// TODO Auto-generated method stub
+		declarer.declare(new Fields("field"));
+	}
+	
 }
